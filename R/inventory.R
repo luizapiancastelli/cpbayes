edd5e8283bd67af879f30ca4452adb90f2bdefe5
Luiza <luiza.piancastelli@ucdconnect.ie>
2022-06-20 15:34:52 +0300

    June 20th: Finished and tested exchange_reg Added takeoverbids and inventory data sets + documentation

---
+++ R/inventory.R
@@ -0,0 +1,10 @@
+#' Inventory data
+#' 
+#' Quarterly sales counts of clothing items in 3168 stores.
+#' 
+#' @source Shmueli et al. (2005)
+#' @references 
+#' Shmueli et al (2005). 
+#' A useful distribution for fitting discrete data: Revival of the Conway-Maxwell-Poisson distribution. 
+#' Journal of the Royal Statistical Society: Series C (Applied Statistics) 54(1), 127-142.
+"inventory"
---
+++ R/takeoverbids.R
@@ -0,0 +1,30 @@
+#' Takeoverbids data
+#' 
+#' Data set from Jaggia and Thosar (1993) on the number of bids received by U.S. firms that were
+#' targets of bid offers during 1978-1985 and taken over within 1 year of the initial offer. 
+#' The response variable is the number of takeover bids excluding the initial bid for a particular company.
+#' Multiple covariates are available with descriptions below from Saez-Castillo & Conde-Sanchez, A. (2013).
+#' 
+#' @format
+#' \describe{
+#' \item{numbids}{Count of takeover bids excluding the initial bid.}
+#' \item{bidprem}{Bid price divided by price 14 working days before bid.}
+#' \item{finrest}{Did management propose changes in ownership structure? (1=YES,0=NO)}
+#' \item{insthold}{Percentage of stock held by institutions.}
+#' \item{leglrest}{Indicator variable for legal defence by lawsuit.}
+#' \item{rearest}{Did management propose changes in asset structure? (1=YES,0=NO)}
+#' \item{regulatn}{Did federal regulators intervene? (1=YES,0=NO)}
+#' \item{size}{Total book value of assets. (USD billions)}
+#' \item{takeover}{Indicates if the company was being taken over.}
+#' \item{weeks}{Time in weeks between the initial and final offers.}
+#' \item{whtknght}{Did management invite friendly third-party bid?  (1=YES,0=NO)}
+#' \item{sizesq}{Book value squared.}
+#' }
+#' 
+#' @references 
+#' Jaggia, S. and S. Thosar (1993). Multiple bids as a consequence of target management resistance: 
+#' A count data approach. Review of Quantitative Finance and Accounting 3(4), 447-457.
+#' 
+#' Saez-Castillo, Antonio & Conde-Sanchez, A. (2013). A hyper-Poisson regression model for overdispersed 
+#' and underdispersed count data. Computational Statistics & Data Analysis. 61. 148-157.
+"takeoverbids"
